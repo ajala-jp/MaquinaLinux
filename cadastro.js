f4bbd89dd978602fd3b2cd7d0e0dbf204b379c66
ajala-jp <joaopedroajala07@gmail.com>
2024-08-21 15:32:53 -0400

    Testando java em node

---
+++ cadastro.js
@@ -0,0 +1,6 @@
+var x = 10
+var y = 20
+
+var res = x * y
+
+console.log("Resultado:",+res)
---
+++ pessoa.js
@@ -0,0 +1,21 @@
+const pessoa = {
+
+	'nome': 'João',
+	'idade': 19,
+	'email':'vascodagama@asdadasdasdasdasd.com'
+
+}
+
+let  idade = 19
+
+for(var i = 0; i < 10; i++){
+
+
+	idade += 1
+
+}
+
+console.log("Pessoa: "+pessoa.nome+" tem "+idade+" anos")
+console.log("\n")
+console.log(pessoa.email)
+console.log(typeof(pessoa))
